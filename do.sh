4e256f64cc5467243dd99e67740879235af1be21
Ismail Mazumder <im087921@gmail.com>
2025-06-04 16:43:39 +0600

    bari

--- do.sh
+++ do.sh
@@ -2,7 +2,9 @@
 # sudo dd if=bootloader.bin of=/dev/sdX bs=512 count=1
 # sudo dd if=image.raw of=/dev/sdX bs=512 seek=1
 # sudo qemu-system-x86_64 -fda /dev/sdX
-nasm -f bin main.asm -o bootloader.bin
-sudo dd if=bootloader.bin of=/dev/sdc1 bs=512 count=1
-sudo dd if=image.raw of=/dev/sdc1 bs=512 seek=1
-sudo qemu-system-x86_64 -fda /dev/sdc1
+# nasm -f bin main.asm -o bootloader.bin
+# sudo dd if=bootloader.bin of=/dev/sdc1 bs=512 count=1
+# sudo dd if=image.raw of=/dev/sdc1 bs=512 seek=1
+# sudo qemu-system-x86_64 -fda /dev/sdc1
+nasm -f bin main.asm -o boot.bin
+qemu-system-x86_64 boot.bin
